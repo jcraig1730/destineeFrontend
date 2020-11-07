97e5933ea8a5841bd07ac87528f0d3a2e03dce46
Jared Craig <jcraig1730@gmail.com>
2020-11-06 22:00:27 -0600

    Add Footer component; install fontawesome

---
+++ components/Footer/index.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import styles from "./footer.module.scss";
+import Link from "next/link";
+
+const Footer = (props) => {
+  return (
+    <div className={styles.footer}>
+      <div className={styles.followLinks}>
+        <div className={styles.followLink}>
+          <Link href="">
+            <FontAwesomeIcon icon={["fab", "twitter"]} />
+          </Link>
+        </div>
+        <div className={styles.followLink}>
+          <Link href="">
+            <FontAwesomeIcon icon={["fab", "facebook"]} />
+          </Link>
+        </div>
+        <div className={styles.followLink}>
+          <Link href="">
+            <FontAwesomeIcon icon={["fab", "pinterest"]} />
+          </Link>
+        </div>
+        <div className={styles.followLink}>
+          <Link href="">
+            <FontAwesomeIcon icon={["fab", "instagram"]} />
+          </Link>
+        </div>
+        <div className={styles.followLink}>
+          <Link href="">
+            <FontAwesomeIcon icon={["fab", "linkedin"]} />
+          </Link>
+        </div>
+      </div>
+      <div className={styles.tag}>
+        Made by{" "}
+        <span className={styles.hand}>
+          <FontAwesomeIcon icon={["fas", "hand-sparkles"]} />
+        </span>{" "}
+        with{" "}
+        <span className={styles.heart}>
+          <FontAwesomeIcon icon={["fas", "heart"]} />
+        </span>{" "}
+        by Destinee
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- components/Layout/index.jsx
+++ components/Layout/index.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import styles from "./layout.module.scss";
 import Navbar from "../Navbar";
+import Footer from "../Footer";
 
 const Layout = (props) => {
   return (
     <div className={styles.layout}>
       <Navbar />
       {props.children}
+      <Footer />
     </div>
   );
 };
