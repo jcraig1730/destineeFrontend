ca975e0eca9c0a4f5f44765877c9ffe6f1ff213f
Jared Craig <jcraig1730@gmail.com>
2020-11-12 12:31:27 -0600

    Add coming soon placeholders to art and about pages

---
+++ pages/about.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "../styles/about.module.scss";
+
+const About = (props) => {
+  return <div className={styles.about}>Coming Soon!</div>;
+};
+
+export default About;
---
+++ pages/art.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "../styles/art.module.scss";
+
+const Art = (props) => {
+  return <div className={styles.art}>Coming Soon!</div>;
+};
+
+export default Art;
